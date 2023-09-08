14cbe3bde76a1326105a32e351236565e0844c85
Aman chaudhary <123990844+theamanchaudhary@users.noreply.github.com>
2023-09-08 23:52:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /BackupBot
 fi
-cd /Auto-search-tamil-bot
+cd /BackupBot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
