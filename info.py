6de47ac48ab09d23f54739c69d607f9c9b9489b0
Aman chaudhary <123990844+theamanchaudhary@users.noreply.github.com>
2023-09-08 23:20:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,9 +43,9 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://ashupatil62611:DH6bdsudNKz9oSgH@cluster0.zqbv07r.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Telegram_filesg")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_filesg')
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://theamanchaudhary:updatesbyaman@cluster0.jgusv6m.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
